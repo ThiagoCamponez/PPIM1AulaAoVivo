52d97dce0891dc405e2bddb14febb6bb0761fc31
ThiagoCamponez <thiagotradercamponez@gmail.com>
2024-02-18 22:28:25 -0300

    Concluido o projeto

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 import express from 'express';
+import process from 'process';
+import path from 'path';
+import session from 'express-session';
+import autenticar from './seguranca/autenticar.js';
 
-const host='0.0.0.0'; // Este ip representa todas as interfaces (placas de rede) do computador
-const porta = 3000; // Esta porta identifica um programa em execução no host hospedeiro
+const host='0.0.0.0'; //O ip 0.0.0.0 representa todas as interfaces (placas de rede) do computador onde essa aplicação for executada
+const porta = 3000;  //Porta identifica um programa em execução no host hospedeiro
 
 const app = express();
 
-app.get('/', (requisicao, resposta) => {
-    resposta.write('<h1>Seja bem vindo ao nosso site!</h1>')
-    resposta.end();
-} ); //Arrow function
+app.use(express.urlencoded({extended: true})); //biblioteca qs
 
-app.get('/index.html', (requisicao, resposta) => {
-    resposta.write('<h1>Esse é o nosso index.html</h1>')
-    resposta.end();
-});
+//gerencie uma sessão como sendo uma espécie de memória adquirida pelo servidor para lembrar com quem ele conversa
+app.use(session({
+    secret: 'M1nH4Ch4v3S3cr3t4',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {  
+        maxAge: 60 * 1000 * 15
+    }
+}))
+
+app.post('/login', (requisicao, resposta)=>{
+    const usuario = requisicao.body.usuario;
+    const senha   = requisicao.body.senha;
+    if (usuario && senha && usuario === 'ThiagoCamponez' && senha === '123'){
+        requisicao.session.usuarioLogado = true;
+        resposta.redirect('/cadastroCliente.html');
+    }
+    else{
+        resposta.redirect('/login.html');
+    }
+})
+
+//O express oferece funcionalidades para permitir que conteúdo estático seja fornecido
+app.use(express.static(path.join(process.cwd(), 'publico')));
+
+app.use(autenticar, express.static(path.join(process.cwd(), 'privado')));
 
 app.listen(porta, host, ()=>{
     console.log(`Servidor escutando em http://${host}:${porta}`);
